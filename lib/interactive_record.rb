51500f013ad3c811e7b9b9379b16c2a6141919bf
Justin Kenney <justinkenney13@gmail.com>
2018-11-02 15:25:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,8 +17,12 @@ class InteractiveRecord
     # self.column_names.each{|col_name| attr_accessor col_name.to_s}
   end
 
-  def add_attr_accessors
-    self.column_names.each{|col_name| attr_accessor col_name.to_s}
+  # def add_attr_accessors
+    self.column_names.each do |col_name|
+      binding.pry
+      attr_accessor col_name.to_s
+    end
+    
   end
 
 
