eb3b1e474c2bc73edd2a464b89aa1fddecf7be33
Justin Kenney <justinkenney13@gmail.com>
2018-11-02 16:15:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -45,7 +45,7 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    binding.pry
+    # binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
 
     DB[:conn].execute(sql)
@@ -59,7 +59,7 @@ class InteractiveRecord
 
     val = DB[:conn].execute(sql)
 
-    # binding.pry
+    binding.pry
 
 
 
