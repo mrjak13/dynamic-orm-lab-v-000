58944f453efc841ef5744b5128092bca133d3cd0
Justin Kenney <justinkenney13@gmail.com>
2018-11-02 15:01:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,15 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+    
+  end
+
+  def self.column_names
+    
+  end
+
+  
   
 end
